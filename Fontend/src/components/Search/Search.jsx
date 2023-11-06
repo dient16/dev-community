2212e6f3ecbbfa22ed6b39bca70bd1ffc4e28ed8
dient16 <diendev16@gmail.com>
2023-11-06 08:15:55 +0700

    component search

--- Fontend/src/components/Search/Search.jsx
+++ Fontend/src/components/Search/Search.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Search.scss';
+import icons from '~/utils/icons';
+import { Popover } from 'antd';
+import SearchResult from './SearchResult';
 
 const Search = () => {
+    const { BiSearch, PiCircleNotchBold, IoIosCloseCircle } = icons;
+    const [isInputFocused, setIsInputFocused] = useState(false);
+    const isLoading = false;
+
+    const handleInputFocus = () => {
+        setIsInputFocused(true);
+    };
+
+    const handleInputBlur = () => {
+        setIsInputFocused(false);
+    };
+
     return (
-        <div>
-            <input type="text" placeholder="Search post" />
-        </div>
+        <Popover content={<SearchResult />} open={isInputFocused} arrow={false}>
+            <div className="search-header">
+                <input
+                    className="search-header__input"
+                    type="text"
+                    placeholder="Search post"
+                    onFocus={handleInputFocus}
+                    onBlur={handleInputBlur}
+                />
+                <span className="search-header__loading">
+                    <IoIosCloseCircle />
+                </span>
+                {isLoading && (
+                    <span className="search-header__clear">
+                        <PiCircleNotchBold />
+                    </span>
+                )}
+                <button className="search-header__btn">
+                    <BiSearch size={20} />
+                </button>
+            </div>
+        </Popover>
     );
 };
 
